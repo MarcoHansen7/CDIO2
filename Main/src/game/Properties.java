056e5f74188855516276f67b9057881ea590925b
Sofiedige <sofiedige1@gmail.com>
2021-10-18 13:34:46 +0200

    kode til bank
    kode til properties

---
+++ Main/src/game/Properties.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+package game;
+
+public class Properties {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] propertyList = {0, 250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
+        int numbers;
+        numbers = sc.nextInt();
+        System.out.println(numbers);
+
+    }
+}

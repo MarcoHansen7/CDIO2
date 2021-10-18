6f66f7e27e290480ec8b4a0256b3b090bd381c53
anshjyotsingh <anshjyot1504@gmail.com>
2021-10-18 13:22:44 +0200

    Bank og Player

---
+++ Main/src/game/Bank.java
@@ -0,0 +1,23 @@
+package game;
+
+import java.util.Scanner;
+
+public class Bank {
+    //Bank b= new Bank();
+    int amount = 1000;
+
+    public Bank(int i) {
+    }
+
+    public int getBalance(){
+        return amount;
+    }
+    public void add(int amt){
+        amount = amount + amt;
+        System.out.println("Total Balance: " +amount);
+
+        //add(amt);
+
+    }
+
+}

8885da0d76c5134e33c6be623837dca10d983ccc
nicktahmasebi <s195099@student.dtu.dk>
2021-10-18 15:18:39 +0200

    Properties Virker - Nick

--- Main/src/game/Properties.java
+++ Main/src/game/Properties.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 public class Properties {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
-        int test;
+        int balance = 0;
         int[] propertyList = {0, 250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
         int numbers;
         numbers = sc.nextInt();
-        numbers--;
         System.out.println(numbers);
-        System.out.println(propertyList [numbers]);
+        System.out.println(propertyList[numbers-1]);
 
-        test = 1 + propertyList [numbers];
-        System.out.println
+
+        balance += propertyList [numbers-1];
+        System.out.println(balance);
+
+        numbers = sc.nextInt();
+        System.out.println(numbers);
+
+        balance += propertyList [numbers-1];
+        System.out.println(balance);
     }
 }

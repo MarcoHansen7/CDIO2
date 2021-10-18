67f7be10a82ae903156a878a4c1d8bf5b89ce358
Sofiedige <sofiedige1@gmail.com>
2021-10-18 14:54:16 +0200

    kode til bank
    kode til properties 2

--- Main/src/game/Properties.java
+++ Main/src/game/Properties.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class Properties {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
+        int test;
         int[] propertyList = {0, 250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
         int numbers;
         numbers = sc.nextInt();
+        numbers--;
         System.out.println(numbers);
+        System.out.println(propertyList [numbers]);
 
+        test = 1 + propertyList [numbers];
+        System.out.println
     }
 }

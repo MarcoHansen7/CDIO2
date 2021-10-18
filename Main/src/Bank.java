5edd9d987de7418d7fbb1fe1a04da1004081a9bf
Sofiedige <sofiedige1@gmail.com>
2021-10-18 12:54:36 +0200

    kode til bank

--- Main/src/Bank.java
+++ Main/src/Bank.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
+
 public class Bank {
+    //Bank b= new Bank();
+    int amount = 1000;
 
+    public int getBalance(){
+    return amount;
+}
+public void add(int amt){
+    amount = amount + amt;
+    System.out.println("Total Balance: " +amount);
 
+    //add(amt);
+
+}
+
+}
+
+class Deposit{
+    int amt=0;
+    public int userInput()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the amount to be deposited: ");
+        amt = scanner.nextInt();
+    }
+    return amt;
 }

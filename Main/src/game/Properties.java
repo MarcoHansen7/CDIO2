bdf808f411b14801df52bf24cf7ebe4d19af0461
MarcoHansen7 <s194302@student.dtu.dk>
2021-10-18 13:41:24 +0200

    TjekDiceVirker

--- Main/src/game/Properties.java
+++ Main/src/game/Properties.java
@@ -1,8 +1,9 @@
-import java.util.Scanner;
 package game;
+import java.util.Scanner;
+
 
 public class Properties {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] propertyList = {0, 250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
         int numbers;

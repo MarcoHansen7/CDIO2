5607165dd1b0b4ecbaa8b581b28f99317a7d556a
anshjyotsingh <anshjyot1504@gmail.com>
2021-10-28 15:51:15 +0200

    BankTest completed, CupTest completed

---
+++ Test/src/testing/BankTest.java
@@ -0,0 +1,35 @@
+package testing;
+import org.junit.Test;
+import game.Bank;
+
+import static org.junit.Assert.*;
+
+public class BankTest {
+
+    public BankTest () { }
+
+    /**
+     * Testing the toString method in the the Bank class, the method should return the "amount"
+     */
+    @Test
+    public void testingToStringMethod() {
+        System.out.println("toString");
+        Bank amount = new Bank(1000);
+        String expectingResult = "(1000)";
+        String actualResult = amount.toString();
+        assertEquals(expectingResult, actualResult);
+    }
+
+    /**
+     * Testing the add-amount method in the Bank class, adding an amount to the overall amount.
+     */
+    @Test
+    public void testingAddMethod() {
+        System.out.println("addEffect");
+        int number = 1000;
+        Bank amount = new Bank(0);
+        amount.add(number);
+
+    }
+
+}
--- Test/src/testing/CupTest.java
+++ Test/src/testing/CupTest.java
@@ -1,4 +1,23 @@
 package testing;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import game.Cup;
 
 public class CupTest {
+
+    /**
+     * Testing the Cup and dice, the method should roll two dices and its combined sum should be limited from
+     * 2-12, as it is: two six sided dice
+     */
+    @Test
+    public void test() {
+
+        Cup cup = new Cup();
+        int result = cup.sum();
+        if (result > 12 || result < 2) {
+            fail("The dice is not limited ");
+        }
+
+    }
+
 }
